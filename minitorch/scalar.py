4dbe4d8c52c08820d5a4a9303cc7f08782d24b23
Jerin Philip <jerinphilip@live.in>
2022-10-23 16:27:25 +0530

    Implement scalar::central_difference

--- minitorch/scalar.py
+++ minitorch/scalar.py
@@ -22,8 +22,11 @@ def central_difference(f, *vals, arg=0, epsilon=1e-6):
     Returns:
         float : An approximation of :math:`f'_i(x_0, \ldots, x_{n-1})`
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError("Need to implement for Task 1.1")
+    x = list(vals)
+    x_h = list(vals)
+    x_h[arg] += epsilon
+
+    return (f(*x_h) - f(*x)) / epsilon
 
 
 # ## Task 1.2 and 1.4
